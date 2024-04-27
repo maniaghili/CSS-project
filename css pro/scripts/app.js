60863c98359661e836349fbe01e47af34a52e60c
maniaghili <aligholimani@gmail.com>
2024-04-27 11:32:27 +0330

    add hide function

--- css pro/scripts/app.js
+++ css pro/scripts/app.js
@@ -2,7 +2,12 @@ console.log('salammmmm')
 let $ = document
 let concat_os = $.querySelector('.header__link')
 let modal = $.querySelector('.salam')
+let X = $.querySelector('#X')
 let oo = concat_os.addEventListener('click',function(e){
     e.preventDefault()
     modal.style.display = `inline`
+})
+let hidemodal = X.addEventListener('click',function(){
+    modal.style.display = `none`
+
 })
